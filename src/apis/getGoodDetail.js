7a880be28e0d65a29acb0217c2e5c0bec6ba2e62
migu <migu@fanhaoyue.com>
2020-01-02 16:22:01 +0800

    http修改为兼容https模式

--- src/apis/getGoodDetail.js
+++ src/apis/getGoodDetail.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export default (productId, groupId)=> axios.get(`http://xh.zjlhhs.com.cn/wap/haibao/product/detail`, {
+export default (productId, groupId)=> axios.get(`//xh.zjlhhs.com.cn/wap/haibao/product/detail`, {
   params: {
     productId,
     groupId
